d989d6c223d6a9ad4c3e9fd68b0a44a96a32dca5
xenon <forlanfr_8@hotmail.fr>
2019-02-04 15:06:46 +0100

    parameters config

--- safeVisibility/settings.py
+++ safeVisibility/settings.py
@@ -149,7 +149,18 @@ STATICFILES_DIRS = [
 # heroku 
 # static files storage in prod env
 if os.environ.get('ENV') == 'PRODUCTION':
+     # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
     STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+    
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
